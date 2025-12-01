cdc9ceec618f6d67c231d8035fe3b6951c87a9b6
KHANT MAUNG MAUNG <30821384+khantmgmg@users.noreply.github.com>
2025-12-01 20:22:29 +0630

    Update userID to a new UUID

--- src/worker-vless.js
+++ src/worker-vless.js
@@ -4,7 +4,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
-let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
+let userID = '7dc319c0-fba4-4252-bcdb-87dbcc20afa5';
 
 let proxyIP = '';
 
